c440ce4a4ae39a1ec3aef157190b0ea2996b9c95
Fredrik Svensson <fs222id@student.lnu.se>
2023-10-14 19:14:01 +0200

    Add form component

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -3,12 +3,17 @@
  */
 
 import HomePage from '../components/homePage/HomePage.js'
+import DateForm from '../components/DateForm/DateForm.js'
 
 class HomeView {
   constructor() {
-    const homePage = new HomePage()
     const appContainer = document.getElementById('app')
+    
+    const homePage = new HomePage()
     appContainer.appendChild(homePage)
+
+    const dateForm = new DateForm()
+    appContainer.appendChild(dateForm)
   }
 }
 
