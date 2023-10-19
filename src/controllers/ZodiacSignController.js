8fc1031c189815777c26c70e5ba6a2c9391fa287
Fredrik Svensson <fs222id@student.lnu.se>
2023-10-19 14:36:18 +0200

    Add controller...

--- src/controllers/ZodiacSignController.js
+++ src/controllers/ZodiacSignController.js
@@ -1,4 +1,17 @@
 /**
  * Handles the requests for a zodiac sign.
  */
+import ZodiacSign from '../models/ZodiacSign.js'
 
+class ZodiacSignController {
+  
+    constructor() {
+      this.zodiacSign = new ZodiacSign()
+    }
+  
+    getZodiacSign(date) {
+      return this.zodiacSign.getZodiacSign(date)
+    }
+}
+
+export default ZodiacSignController
