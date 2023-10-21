493a4e079abc7e3d6e5411f28ea3ecb69f06b37e
Fredrik Svensson <fs222id@student.lnu.se>
2023-10-21 16:54:24 +0200

    Add display of actual sign

--- src/components/CurrentZodiacSign/CurrentZodiacSign.js
+++ src/components/CurrentZodiacSign/CurrentZodiacSign.js
@@ -4,18 +4,24 @@
  */
 
 import ZodiacSignController from '../../controllers/ZodiacSignController.js'
+import CelestialFinderController from '../../controllers/CelestialFinderController.js'
 
 const template = document.createElement('template')
 template.innerHTML = `
   <div id="current-zodiac-sign">
-    <p>The current Zodiac Sign:</p>
+    <p>The current Zodiac Sign</p>
+    <p>is traditionally</p>
     <div id="current-zodiac-sign-name"></div>
+    <br>
+    <p>But the sun is actually in</p>
+    <div id="actual-zodiac-sign-name"></div>
   </div>
 `
 
 class CurrentZodiacSign extends HTMLElement {
   #currentZodiacSign
   #currentZodiacSignName
+  #actualZodiacSignName
 
   constructor() {
     super()
@@ -28,6 +34,11 @@ class CurrentZodiacSign extends HTMLElement {
     this.#currentZodiacSign = this.shadowRoot.querySelector('#current-zodiac-sign')
     this.#currentZodiacSignName = this.shadowRoot.querySelector('#current-zodiac-sign-name')
     this.#currentZodiacSignName.innerText = zodiacSignController.getZodiacSign(new Date()).name
+
+    let celestialFinderController = new CelestialFinderController()
+
+    this.#actualZodiacSignName = this.shadowRoot.querySelector('#actual-zodiac-sign-name')
+    this.#actualZodiacSignName.innerText = celestialFinderController.getZodiacSignWithSun('2023-10-21').name
 
   }
 }
