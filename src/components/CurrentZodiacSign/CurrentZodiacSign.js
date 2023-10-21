d381ee5271e7acb88934a791353e10d56f966955
Fredrik Svensson <fs222id@student.lnu.se>
2023-10-21 17:02:58 +0200

    Display actual sign

--- src/components/CurrentZodiacSign/CurrentZodiacSign.js
+++ src/components/CurrentZodiacSign/CurrentZodiacSign.js
@@ -29,17 +29,18 @@ class CurrentZodiacSign extends HTMLElement {
       template.content.cloneNode(true)
     )
 
+    let today = new Date()
+
     let zodiacSignController = new ZodiacSignController()
 
     this.#currentZodiacSign = this.shadowRoot.querySelector('#current-zodiac-sign')
     this.#currentZodiacSignName = this.shadowRoot.querySelector('#current-zodiac-sign-name')
-    this.#currentZodiacSignName.innerText = zodiacSignController.getZodiacSign(new Date()).name
+    this.#currentZodiacSignName.innerText = zodiacSignController.getZodiacSign(today).name
 
     let celestialFinderController = new CelestialFinderController()
-
+    let dateCelestialFinderFormat = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
     this.#actualZodiacSignName = this.shadowRoot.querySelector('#actual-zodiac-sign-name')
-    this.#actualZodiacSignName.innerText = celestialFinderController.getZodiacSignWithSun('2023-10-21').name
-
+    this.#actualZodiacSignName.innerText = celestialFinderController.getZodiacSignWithSun(dateCelestialFinderFormat).name
   }
 }
 
