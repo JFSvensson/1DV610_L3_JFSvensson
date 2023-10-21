a2b258ecc4865ecb2f4b4605e3977bee29a2b926
Fredrik Svensson <fs222id@student.lnu.se>
2023-10-22 01:06:59 +0200

    Fix after rename and set/get

--- src/components/CurrentZodiacSign/CurrentZodiacSign.js
+++ src/components/CurrentZodiacSign/CurrentZodiacSign.js
@@ -3,8 +3,8 @@
  *
  */
 
-import ZodiacSignController from '../../controllers/ZodiacSignController.js'
 import CelestialFinderController from '../../controllers/CelestialFinderController.js'
+import ZodiacSigns from '../../models/ZodiacSigns.js'
 
 const template = document.createElement('template')
 template.innerHTML = `
@@ -29,18 +29,19 @@ class CurrentZodiacSign extends HTMLElement {
       template.content.cloneNode(true)
     )
 
+    let zodiacSigns = new ZodiacSigns()
     let today = new Date()
-
-    let zodiacSignController = new ZodiacSignController()
+    zodiacSigns.setZodiacSign(today)
 
     this.#currentZodiacSign = this.shadowRoot.querySelector('#current-zodiac-sign')
     this.#currentZodiacSignName = this.shadowRoot.querySelector('#current-zodiac-sign-name')
-    this.#currentZodiacSignName.innerText = zodiacSignController.getZodiacSign(today).name
+    this.#currentZodiacSignName.innerText = zodiacSigns.getZodiacSign().name
 
     let celestialFinderController = new CelestialFinderController()
     let dateCelestialFinderFormat = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+    celestialFinderController.setZodiacSignWithSun(dateCelestialFinderFormat)
     this.#actualZodiacSignName = this.shadowRoot.querySelector('#actual-zodiac-sign-name')
-    this.#actualZodiacSignName.innerText = celestialFinderController.getZodiacSignWithSun(dateCelestialFinderFormat).name
+    this.#actualZodiacSignName.innerText = celestialFinderController.getZodiacSignWithSun().name
   }
 }
 
