cd8c0d05dc38ab3896053f5d9543354ed6bbcf2b
Fredrik Svensson <fs222id@student.lnu.se>
2023-10-21 23:37:27 +0200

    Cleanup

--- src/controllers/CelestialFinderController.js
+++ src/controllers/CelestialFinderController.js
@@ -10,30 +10,24 @@ class CelestialFinderController {
   }
 
   getZodiacSignWithSun(date) {
-    console.log(date)
     this.celestialFinder = new CelestialFinder(date, 0)
     let solarPosition = this.celestialFinder.positionOfTheSun()
     let solarPositionRightAscension = 
       solarPosition.rightAscension.hours 
       + solarPosition.rightAscension.minutes / 60 
       + solarPosition.rightAscension.seconds / 3600
-    console.log(solarPositionRightAscension)
 
     for (const sign of this.zodiacSign.zodiacSigns) {
-      console.log(sign)
       let signLowerBoundaryRightAscension = 
         sign.lowerBoundary.rightAscension.hours 
         + sign.lowerBoundary.rightAscension.minutes / 60 
         + sign.lowerBoundary.rightAscension.seconds / 3600
-      console.log(signLowerBoundaryRightAscension)
       let signUpperBoundaryRightAscension = 
         sign.upperBoundary.rightAscension.hours 
         + sign.upperBoundary.rightAscension.minutes / 60 
         + sign.upperBoundary.rightAscension.seconds / 3600
-      console.log(signUpperBoundaryRightAscension)
       if (solarPositionRightAscension >= signLowerBoundaryRightAscension 
           && solarPositionRightAscension <= signUpperBoundaryRightAscension) {
-        console.log(sign)
         return sign
       }
     }
